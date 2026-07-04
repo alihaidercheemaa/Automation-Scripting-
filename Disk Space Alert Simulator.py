39f348f495ee17bcde4bc45302bb026ba3e2fc7f
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-04 11:44:05 +0500

    Few Changes

--- Disk Space Alert Simulator.py
+++ Disk Space Alert Simulator.py
@@ -8,11 +8,20 @@ disk_usage = float(input("Enter the disk usage percentage : ").strip())
 
 
 if disk_usage >= 0 and disk_usage <= 69:
-    print(f"status: {server_name} is healthy")
+    if environment == "prod":
+        print(f"status: {server_name} is healthy")
+    else:
+        print(f"status: {server_name} is healthy")
 elif disk_usage >= 70 and disk_usage <= 89:
-    print(f"status: {server_name} has warning high disk usage")
+    if environment == "prod":
+        print(f"status: {server_name} has warning high disk usage")
+    else:
+        print(f"status: {server_name} check it before it went to production")
 elif disk_usage >= 90 and disk_usage <= 100:
-    print(f"status: {server_name} is critical")
+    if environment == "prod":
+        print(f"status: {server_name} is critical")
+    else:
+        print(f"status: {server_name} check it before it went to production")
 else:
     print("Error: Invalid disk usage percentage entered. Please enter a value between 0 and 100.")
 
