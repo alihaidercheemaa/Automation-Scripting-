1a3521ea09dcf51b4a362d4ed7b8eb35e5b8a285
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-04 11:25:46 +0500

    Adding conditions

--- Disk Space Alert Simulator.py
+++ Disk Space Alert Simulator.py
@@ -1,7 +1,13 @@
+
 server_name = input("Enter the server name : ").strip().lower()
 disk_usage = float(input("Enter the disk usage percentage : ").strip())
 
 if disk_usage >= 0 and disk_usage <= 69:
-    print(f"Alert: {server_name} has heaalthy disk usage: {disk_usage}%")
+    print(f"Alert: {server_name} has healthy disk usage: {disk_usage}%")
 elif disk_usage >= 70 and disk_usage <= 89:
-    print(f"Info: {server_name} has normal disk usage: {disk_usage}%")
+    print(f"Info: {server_name} has high disk usage: {disk_usage}%")
+elif disk_usage >= 90 and disk_usage <= 100:
+    print(f"Warning: {server_name} has critical disk usage: {disk_usage}%")
+else:
+    print("Error: Invalid disk usage percentage entered. Please enter a value between 0 and 100.")
+    
