11af5a5cb0b3a8e5e3d513919b92fe3e5b9db525
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-05 05:15:57 +0500

    Changes

--- Server Ping Simulator.py
+++ Server Ping Simulator.py
@@ -1,8 +1,21 @@
 # Project: Disk Space Alert Simulator
 # Author: Ali Haider
 # Description:
-# Ping mutiple server in a list using loops
+# Ping multiple servers in a list using loops
 
+servers = [
+    "web-prod-01",
+    "web-prod-02",
+    "db-prod-01",
+    "api-prod-01",
+    "cache-prod-01",
+    "backup-prod-01",
+    "proxy-prod-01",
+    "worker-prod-01",
+    "monitor-prod-01",
+    "storage-prod-01"
+]
 
-for i in range(2,10,2):
-    print(i)
+for server in servers:
+    print(f"Pinging server: {server}...")
+    print(f"Status: Successfully")
