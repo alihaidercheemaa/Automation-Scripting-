901adf4a0bd913ebd02200b5832b1a2a3dded61e
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-06 05:14:49 +0500

    Adding the scenario

--- infrastructure_heath_monitor.py
+++ infrastructure_heath_monitor.py
@@ -2,3 +2,7 @@
 # Author: Ali Haider   
 # Date: 2024-06-15
 
+''' Scenario
+    Imagine you're a Junior DevOps Engineer.
+    Your team has 10 Linux servers.
+    Your task is to write a Python automation script that checks each server and generates a health report.'''
