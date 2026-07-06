9dbc7951dc29720b10d0beade6e308a85de97f7d
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-06 05:22:26 +0500

    Adding libraries and setting of the better CLI output

--- infrastructure_heath_monitor.py
+++ infrastructure_heath_monitor.py
@@ -6,3 +6,16 @@
     Imagine you're a Junior DevOps Engineer.
     Your team has 10 Linux servers.
     Your task is to write a Python automation script that checks each server and generates a health report.'''
+
+import platform
+import os
+from datetime import date, datetime, time
+
+print("=" * 50)
+print("      DevOps Infrastructure Health Monitor")
+print("=" * 50)
+
+print(f"Current Date     : {date.today()}")
+print(f"Current Time     : {datetime.now().strftime('%H:%M:%S')}")
+print(f"Python Version   : {platform.python_version()}")
+print(f"Operating System : {platform.system()} {platform.release()}")
