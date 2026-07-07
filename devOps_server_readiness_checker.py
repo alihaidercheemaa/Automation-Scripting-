999b3395cb319bddb7e969c88486f627a309b2d8
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-07 07:33:28 +0500

    Adding demo data in the file

--- devOps_server_readiness_checker.py
+++ devOps_server_readiness_checker.py
@@ -1,35 +1,90 @@
 # Script for checking whether the server is ready for deployment or not.
 # Author : Ali Haider
 
-server = [
+servers = [
     {
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
 
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     },
     {
-
+    "hostname": "web-prod-01",
+    "ip_address": "192.168.10.10",
+    "environment": "production",
+    "cpu_usage": 45,
+    "ram_usage": 62,
+    "disk_usage": 55,
+    "status": "Running"
     }
 ]
 
-for server in enumerate(server):
-    if server:
+for server in enumerate(servers):
+    if servers:
