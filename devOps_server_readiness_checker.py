0a1b9a6a08fb7bb5339decd3499ceceaa8bd0915
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-07 07:30:24 +0500

    Adding a new script

---
+++ devOps_server_readiness_checker.py
@@ -0,0 +1,35 @@
+# Script for checking whether the server is ready for deployment or not.
+# Author : Ali Haider
+
+server = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+for server in enumerate(server):
+    if server:
