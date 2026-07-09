7bfed7326a8e3d47a153311c3b45c7d958719ce5
alihaidercheemaa <alihaidercheema08@gmail.com>
2026-07-09 17:49:42 +0500

    Adding dummy user data

--- linux_user_management.py
+++ linux_user_management.py
@@ -1,2 +1,38 @@
 # Linux User Management using list, Dictionary, loops and conditional logic
 # Aurthor: Ali Haider
+
+user = [{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},{"username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True},
+    {
+    "username": "...",
+    "role": "...",
+    "status": "...",
+    "sudo": True
+    }
+    ]
